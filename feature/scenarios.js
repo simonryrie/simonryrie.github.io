e620991789c781f553da18bb7463f49aa62e3bfc
simonryrie <simonryrie@gmail.com>
2022-11-12 16:31:31 +0000

    initial commit for feature
    first object created
    plan
    HTML, CSS + JS x2

---
+++ feature/scenarios.js
@@ -0,0 +1,6 @@
+const opening = {
+    text: "You awaken to find yourself lying on some kind of soft furnishing in a small room. The space is dark save for the dim glow emanating from a pile of multi-coloured orbs in its centre. Twinkling ambient music drifts through the air from an unknown source. Although you cannot be certain, you think you sense the presence of other bodies lying around you. On the far side of room, vertical streams of white light enter through a doorway but are quickly overwhelmed by the gloom.",
+    options: ["Approach one of the bodies and ask where you are", "Walk towards the light"]
+    audio:
+    //backdrop: moodlighting()
+}
