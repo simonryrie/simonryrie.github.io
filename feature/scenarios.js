d7c96fc427d12a05802e12049d2a26e29d6865f2
simonryrie <simonryrie@gmail.com>
2022-11-14 21:39:27 +0000

    Scenarios updated
    Buttons not changing bug still to fixed

--- feature/scenarios.js
+++ feature/scenarios.js
@@ -1,22 +1,53 @@
 //scenarios
 
 const opening = {
-    text: "You awaken to find yourself lying on some kind of soft furnishing in a small room. The space is dark except for a dim glow coming from a pile of multi-coloured orbs in its centre. Twinkling ambient music drifts through the air from an unknown source. Although you cannot be certain, you think you sense the presence of other bodies lying around you. On the far side of the room, vertical stripes of white light stream through a doorway but are quickly overwhelmed by the gloom.",
+    text: "You awaken to find yourself lying on soft furnishing in a small room. The space is dark, except for the dim glow of a pile of multi-coloured orbs in its centre. Twinkling ambient music drifts through the air from an unknown source. Although you cannot be certain, you sense the presence of other bodies around you. On the far side of the room, vertical stripes of white light stream through a doorway but are quickly overwhelmed by the gloom.",
     options: ["Approach one of the bodies and ask where you are", "Walk towards the light"],
     audio: "ambient.mp3"
 }
 
 const askBody = {
-    text: "You approach a body and ask where you are. The body seems to sense your presence, but in response you hear only groans. There is nothing intelligible to be found here.",
+    text: "You approach what appears to be a human figure, curled up in the foetal position on some kind of beanbag. The body seems to sense your presence, but in response to your questions you hear only groans. Perhaps it is best left alone - there is nothing intelligible to be found here.",
     options: ["Go and lie down again", "Walk towards the light"],
     audio: "ambient.mp3"
 }
 
 const towardsLight = {
-    text: "You stumble towards the light, feeling unsteady but oddly powerful. As you approach its source, you find it is a doorway covered by colourful dangling beads. You pass through this plastic veil to find yourself in an industrial-looking corridor. Blinking under its harsh lights, you can see a door with a unisex toilet sign to your right. To the left, there are heavy double doors through which you can hear the muffled thump of heavy bass. The metal light fittings vibrate sympathetically.",
+    text: "You stumble towards the light, feeling unsteady but oddly euphoric. As you approach its source, you find it is a doorway covered by colourful dangling beads. You pass through this plastic veil to find yourself in an industrial-looking corridor. Blinking under harsh lights, you can see a door with a unisex toilet sign to your right. To the left, there are heavy double doors through which you can hear the muffled thump of heavy bass. The metal light fittings rattle sympathetically.",
     options: ["Go into the toilet", "Go through the double doors"],
     audio: "corridor.mp3"
 }
+
+const toilets = {
+    text: "Entering the toilet, a smell of strong chemicals passes your nose. Aside from some minor graffiti, the bathroom is in good condition - you recall that you have seen worse. There are four cubicles. Three are empty. From the fourth, you hear two voices speaking in hushed, excited tones. Above the sinks, there is a large mirror surrounded by worn stickers.",
+    options: ["Ask for help", "Look in the mirror"],
+    audio: "toilets.mp3"
+}
+
+const askForHelp = {
+    text: "In the bright lights of the bathroom, you stumble over your words. You manage to blurt out a question, or perhaps it was two questions spoken in the space of one. The hushed voices turn to cautious whispers. There is no help here.",
+    options: ["Look in the mirror", "Leave the bathroom"],
+    audio: "toilets.mp3"
+}
+
+const lookInTheMirror = {
+    text: "As you turn your head towards the mirror, your eyes are drawn to a garishly coloured sticker of a winking sailor. He leers down at you as a speech bubble from his mouth proclaims, 'MILK, MAN'. Moving your gaze towards the centre of the mirror, you see a familiar face. It looks slightly too old to be here. A cold rush of guilt spills from your head downwards and out to your extremities.",
+    options: ["Ask for help", "Leave the bathroom"],
+    audio: "toilets.mp3"
+}
+
+const backToCorridor = {
+    text: "Back into the corridor. From this angle, you can see a piece of paper blu-tacked above the beaded doorway to the room you started in. Printed on it are 3 words: 'Chill Out Room'. From the far end of the corridor, you feel a deep rumble, more in your chest than your ears - the irresistible call of the bass. You feel at once very tired and possibly more awake than you have ever felt in your life.",
+    options: ["Chill out", "Head towards the bass"],
+    audio: "corridor.mp3"
+}
+
+const mainRoom = {
+    text: "It takes all your strength to push open the heavy doors. As you pass the threshold, your senses are assaulted by a powerful flashing light and the sound of pounding, repetitive beats. In the strobing light, you catch monochrome snapshots of writhing bodies engaged in some kind of dance. The air is hot, but you feel a desire to join the sweating mass. In the far corner of the room, a red 'Exit' light glows dimly above a doorway.",
+    options: ["Go to the dancefloor", "Go to the exit"],
+    audio: "mainroom.mp3"
+}
+
 
 //letter by letter function
 
@@ -63,9 +94,12 @@ function toggleMute() {
 
 //change buttons inner HTML function
 
+let buttonOne = document.getElementById("optionOne");
+let buttonTwo = document.getElementById("optionTwo");
+
 function changeButtons(optionOne, optionTwo) {
-document.getElementById("optionOne").innerHTML = optionOne;
-document.getElementById("optionTwo").innerHTML = optionTwo;
+buttonOne.innerHTML = optionOne;
+buttonTwo.innerHTML = optionTwo;
 }
 
 //initialise
@@ -81,26 +115,74 @@ changeButtons(opening.options[0], opening.options[1])
 
 // interactivity scenarios tree
 
-let buttonOne = document.getElementById("optionOne");
-let buttonTwo = document.getElementById("optionTwo");
+switch (buttonOne.innerHTML) {
+    case "Approach one of the bodies and ask where you are":
+        buttonOne.addEventListener("click", function() {
+            currentScenario = askBody
+            text.innerHTML = ""
+            buttonOne.style.visibility = "hidden";
+            buttonTwo.style.visibility = "hidden";
+            letterByLetter("text", currentScenario.text, 30);
+            changeButtons(currentScenario.options[0], currentScenario.options[1]);
+        })
+        buttonTwo.addEventListener("click", function() {
+            currentScenario = towardsLight
+            text.innerHTML = ""
+            buttonOne.style.visibility = "hidden";
+            buttonTwo.style.visibility = "hidden";
+            letterByLetter("text", currentScenario.text, 30);
+            changeButtons(currentScenario.options[0], currentScenario.options[1]);
+            if(document.getElementById("audio").paused) {
+                updateSource(currentScenario.audio);
+                }
+                else{
+                    updateSource(currentScenario.audio);
+                    document.getElementById("audio").play();
+                }
+        });
+        break;
+    case "Go and lie down again":
+        buttonOne.addEventListener("click", function() {
+            currentScenario = opening
+            text.innerHTML = ""
+            buttonOne.style.visibility = "hidden";
+            buttonTwo.style.visibility = "hidden";
+            letterByLetter("text", currentScenario.text, 30);
+            changeButtons(currentScenario.options[0], currentScenario.options[1]);
+        })
+        buttonTwo.addEventListener("click", function() {
+            currentScenario = towardsLight
+            text.innerHTML = ""
+            buttonOne.style.visibility = "hidden";
+            buttonTwo.style.visibility = "hidden";
+            letterByLetter("text", currentScenario.text, 30);
+            changeButtons(currentScenario.options[0], currentScenario.options[1]);
+            if(document.getElementById("audio").paused) {
+                updateSource(currentScenario.audio);
+                }
+                else{
+                    updateSource(currentScenario.audio);
+                    document.getElementById("audio").play();
+                }
+        })
+        break;        
+}
 
-if(currentScenario === opening) {
-    console.log("one")
+/*
+if(currentScenario == opening) {
     buttonOne.addEventListener("click", function() {
         currentScenario = askBody
         text.innerHTML = ""
-        document.getElementById("optionOne").style.visibility = "hidden";
-        document.getElementById("optionTwo").style.visibility = "hidden";
+        buttonOne.style.visibility = "hidden";
+        buttonTwo.style.visibility = "hidden";
         letterByLetter("text", currentScenario.text, 30);
         changeButtons(currentScenario.options[0], currentScenario.options[1]);
-        console.log(currentScenario)
-        return currentScenario = askBody
     })
     buttonTwo.addEventListener("click", function() {
         currentScenario = towardsLight
         text.innerHTML = ""
-        document.getElementById("optionOne").style.visibility = "hidden";
-        document.getElementById("optionTwo").style.visibility = "hidden";
+        buttonOne.style.visibility = "hidden";
+        buttonTwo.style.visibility = "hidden";
         letterByLetter("text", currentScenario.text, 30);
         changeButtons(currentScenario.options[0], currentScenario.options[1]);
         if(document.getElementById("audio").paused) {
@@ -113,23 +195,20 @@ if(currentScenario === opening) {
     })
 }
 
-console.log(currentScenario)
-
-if(currentScenario === askBody) {
-    console.log("yay")
+else if(currentScenario == askBody) {
     buttonOne.addEventListener("click", function() {
         currentScenario = opening
         text.innerHTML = ""
-        document.getElementById("optionOne").style.visibility = "hidden";
-        document.getElementById("optionTwo").style.visibility = "hidden";
+        buttonOne.style.visibility = "hidden";
+        buttonTwo.style.visibility = "hidden";
         letterByLetter("text", currentScenario.text, 30);
         changeButtons(currentScenario.options[0], currentScenario.options[1]);
     })
     buttonTwo.addEventListener("click", function() {
         currentScenario = towardsLight
         text.innerHTML = ""
-        document.getElementById("optionOne").style.visibility = "hidden";
-        document.getElementById("optionTwo").style.visibility = "hidden";
+        buttonOne.style.visibility = "hidden";
+        buttonTwo.style.visibility = "hidden";
         letterByLetter("text", currentScenario.text, 30);
         changeButtons(currentScenario.options[0], currentScenario.options[1]);
         if(document.getElementById("audio").paused) {
@@ -141,3 +220,4 @@ if(currentScenario === askBody) {
             }
     })
 }
+*/
