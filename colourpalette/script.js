544a5d8e7a07356e0478639393a96ed27b447c4e
simonryrie <simonryrie@gmail.com>
2023-01-02 11:38:36 +0000

    New functions added Similar / complementary colours Check whether text should be black or white Text innerHTML changes to rgb values

--- colourpalette/script.js
+++ colourpalette/script.js
@@ -14,27 +14,77 @@ changeButton.addEventListener("click", changeMultipleColours);
 function changeMultipleColours(){
 
     let newColour = ''
+    let r = 0
+    let g = 0
+    let b = 0
 
     function generateRandomColour() {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        newColour = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+        r = Math.floor(Math.random() * 256);
+        g = Math.floor(Math.random() * 256);
+        b = Math.floor(Math.random() * 256);
+        newColour = 'rgb(' + r + ', ' + g + ', ' + b + ')';     
+        return newColour, r, g, b
+    }
+
+    function generateSimilarColour() {
+        let rAddend = Math.floor(Math.random() * 200) - 100
+        let gAddend = Math.floor(Math.random() * 200) - 100
+        let bAddend = Math.floor(Math.random() * 200) - 100
+        r += rAddend
+        g += gAddend
+        b += bAddend
+        if(r < 0) {r = 0}
+        if(r > 255) {r = 255}
+        if(g < 0) {g = 0}
+        if(g > 255) {g = 255}
+        if(b < 0) {b = 0}
+        if(b > 255) {b = 255}
+        newColour = 'rgb(' + r + ', ' + g + ', ' + b + ')';     
         return newColour
     }
 
-    generateRandomColour()
-    console.log(newColour)
-    one.style.backgroundColor = newColour
-    generateRandomColour()
-    two.style.backgroundColor = newColour
-    generateRandomColour()
-    three.style.backgroundColor = newColour
-    generateRandomColour()
-    four.style.backgroundColor = newColour
-    generateRandomColour()
-    five.style.backgroundColor = newColour
-    generateRandomColour()
-    six.style.backgroundColor = newColour
+    function generateComplementaryColour() {
+        let oldR = r
+        let oldG = g
+        let oldB = b
+        r = oldG
+        g = oldB
+        b = oldR
+        newColour = 'rgb(' + r + ', ' + g + ', ' + b + ')';     
+        return newColour
+    }
 
+    function checkBlackorWhite(e) {
+    (r + g + b) >= 350 ? document.getElementById(e).style.color = "black" : document.getElementById(e).style.color = "white"
+    }
+
+    generateRandomColour()
+    one.style.backgroundColor = newColour
+    document.getElementById("one").innerHTML = newColour
+    checkBlackorWhite("one")
+
+    generateSimilarColour()
+    two.style.backgroundColor = newColour
+    document.getElementById("two").innerHTML = newColour
+    checkBlackorWhite("two")
+    
+    generateComplementaryColour()
+    three.style.backgroundColor = newColour
+    document.getElementById("three").innerHTML = newColour
+    checkBlackorWhite("three")
+    
+    generateSimilarColour()
+    four.style.backgroundColor = newColour
+    document.getElementById("four").innerHTML = newColour
+    checkBlackorWhite("four")
+    
+    generateComplementaryColour()
+    five.style.backgroundColor = newColour
+    document.getElementById("five").innerHTML = newColour
+    checkBlackorWhite("five")
+    
+    generateSimilarColour()
+    six.style.backgroundColor = newColour
+    document.getElementById("six").innerHTML = newColour
+    checkBlackorWhite("six")
 }
